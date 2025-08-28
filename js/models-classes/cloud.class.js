8472c2f6cce0cbc26f23d7b938926216510288de
DominikRapp <rapp.dominik@hotmail.com>
2025-08-28 15:02:42 +0200

    first session el pollo loco

---
+++ js/models-classes/cloud.class.js
@@ -0,0 +1,26 @@
+class Cloud extends MovableObject {
+    y = 20;
+    width = 450;
+    height = 300;
+    
+
+    constructor() {
+        super().loadImage('img/5_background/layers/4_clouds/1.png');
+
+        this.x = Math.random() * 500; // Zahl zwischen 0 und 500
+        this.animate();
+    }
+
+    animate() {
+        this.moveLeft();
+
+    }
+
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
+
+    }
+
+}
---
+++ js/models-classes/movable-object.class.js
@@ -0,0 +1,38 @@
+class MovableObject {
+    x = 120;
+    y = 130;
+    img;
+    height = 150;
+    width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.15;
+    otherDirection = false;
+
+
+    // loadImage('img/test.png');
+    loadImage(path) {
+        this.img = new Image(); // this.img = document.getElementById('image') <img id="image" scr>
+        this.img.src = path;
+    }
+
+
+    loadImages(array) {
+        array.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+
+    moveRight() {
+        console.log('Moving right');
+    }
+
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
+
+    }
+}
